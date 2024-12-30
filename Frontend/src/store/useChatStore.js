34674f4240b7a4fb35a8ad768834d75753e4b3b2
NiravMahukiya <niravmathukiya1507@gmail.com>
2024-12-30 17:57:07 +0530

    chat bubull created

--- Frontend/src/store/useChatStore.js
+++ Frontend/src/store/useChatStore.js
@@ -2,7 +2,7 @@ import { create } from "zustand";
 import toast from "react-hot-toast";
 import { axiosInstance } from "../lib/axios";
 
-export const useChatStore = create((set) => ({
+export const useChatStore = create((set, get) => ({
   messages: [],
   users: [],
   selectedUser: null,
@@ -15,7 +15,7 @@ export const useChatStore = create((set) => ({
       const res = await axiosInstance.get("/messages/users");
       set({ users: res.data });
     } catch (error) {
-      toast.error("this is error "+error.response.data.message);
+      toast.error("this is error " + error.response.data.message);
     } finally {
       set({ isUsersLoading: false });
     }
@@ -35,5 +35,15 @@ export const useChatStore = create((set) => ({
 
   setSelectedUser: async (selectedUser) => {
     set({ selectedUser });
+  },
+
+  sendMessage: async (messageData) => {
+    const { selectedUser, messages } = get();
+    try {
+      const res = await axiosInstance.post(`/messages/send/${selectedUser._id}`,messageData);
+      set({ messages: [...messages, res.data] });
+    } catch (error) {
+      toast.error("error in send message" + error);
+    }
   },
 }));
